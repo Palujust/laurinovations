e2f622d3d0d6e3dc58c39d3bc320f3e4c966fc6f
Palujust <justin.palumbo@hotmail.com>
2014-04-30 20:13:05 -0400

    more updates

--- javascript.js
+++ javascript.js
@@ -1,6 +1,48 @@
 $(document).ready(function(){
 
 
+
+
+$('#homeLink').on({
+    'mouseover' : function() {
+      $(this).attr('src','homehover.png');
+    },
+    mouseout : function() {
+  $(this).attr('src','home.png');
+    }
+  });
+
+$('#aboutLink').on({
+    'mouseover' : function() {
+      $(this).attr('src','abouthover.png');
+    },
+    mouseout : function() {
+  $(this).attr('src','about.png');
+    }
+  });
+
+$("#photoLink").on({
+    'mouseover' : function() {
+      $(this).attr('src','photoshover.png');
+    },
+    mouseout : function() {
+  $(this).attr('src','photos.png');
+    }
+  });
+
+$('#contactLink').on({
+    'mouseover' : function() {
+      $(this).attr('src','contacthover.png');
+    },
+    mouseout : function() {
+  $(this).attr('src','contact.png');
+    }
+  });
+
+
+
+
+
 	$("#menu").hover(function(){
 
 
@@ -16,6 +58,26 @@ $(document).ready(function(){
 
 		);
 
+	});
+	$("#photoLink").click(function(){
+
+
+		$("#photoDiv").ScrollTo();
+	});
+	$("#homeLink").click(function(){
+
+
+		$("#homeDiv").ScrollTo();
+	});
+	$("#contactLink").click(function(){
+
+
+		$("#contactDiv").ScrollTo();
+	});
+	$("#aboutLink").click(function(){
+
+
+		$("#aboutDiv").ScrollTo();
 	});
 	$("#menu").mouseleave(function(){
 
